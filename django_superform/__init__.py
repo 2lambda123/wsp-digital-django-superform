ff11082ecd9b9b3f069e4d1909da1be6b02b0033
Restyled.io <commits@restyled.io>
2024-01-19 09:24:17 +0000

    Restyled by reorder-python-imports

--- django_superform/__init__.py
+++ django_superform/__init__.py
@@ -4,10 +4,16 @@ Author: Gregor Müllegger <gregor@muellegger.de>
 Project home: https://github.com/gregmuellegger/django-superform
 See http://django-superform.readthedocs.org/en/latest/ for complete docs.
 """
-from .fields import (ForeignKeyFormField, FormField, FormSetField,
-                     InlineFormSetField, ModelFormField, ModelFormSetField)
-from .forms import SuperForm, SuperModelForm
-from .widgets import FormSetWidget, FormWidget
+from .fields import ForeignKeyFormField
+from .fields import FormField
+from .fields import FormSetField
+from .fields import InlineFormSetField
+from .fields import ModelFormField
+from .fields import ModelFormSetField
+from .forms import SuperForm
+from .forms import SuperModelForm
+from .widgets import FormSetWidget
+from .widgets import FormWidget
 
 __version__ = "0.5.0"
 
